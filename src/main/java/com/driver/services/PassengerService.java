874902af2816ac690e336ed2443f6ee3c1652926
Vivek Kumar <vickyvivek557@gmail.com>
2023-10-04 19:19:26 +0530

    add passenger

--- src/main/java/com/driver/services/PassengerService.java
+++ src/main/java/com/driver/services/PassengerService.java
@@ -6,6 +6,8 @@ import com.driver.repository.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class PassengerService {
 
@@ -14,7 +16,13 @@ public class PassengerService {
 
     public Integer addPassenger(Passenger passenger){
         //Add the passenger Object in the passengerDb and return the passegnerId that has been returned
-        return null;
+        Passenger newPassanger = new Passenger();
+        newPassanger.setName(passenger.getName());
+        newPassanger.setAge(passenger.getAge());
+        newPassanger.setBookedTickets(new ArrayList<>());
+        // save newPassenger
+        Passenger savedPassenger = passengerRepository.save(newPassanger);
+        return savedPassenger.getPassengerId();
     }
 
 }
